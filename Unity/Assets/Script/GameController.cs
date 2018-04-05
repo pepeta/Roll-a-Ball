525bc532ba5a03a9f692a59d96a53c75e8e7f881
kaori <shiomusubeam@gmail.com>
2018-04-06 01:35:21 +0900

    クリア判定を追加

--- Unity/Assets/Script/GameController.cs
+++ Unity/Assets/Script/GameController.cs
@@ -4,9 +4,16 @@ using System.Collections;
 public class GameController : MonoBehaviour
 {
     public UnityEngine.UI.Text scoreLabel;
+    public GameObject winnerLabelObject;
+
     public void Update()
     {
         int count = GameObject.FindGameObjectsWithTag("Item").Length;
         scoreLabel.text = count.ToString();
+
+        if (count == 0)
+        {
+            winnerLabelObject.SetActive(true);
+        }
     }
 }
