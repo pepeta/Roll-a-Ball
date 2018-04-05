57f9be9be1e0300a71f4e80c67acb5bbf6d3ce45
kaori <shiomusubeam@gmail.com>
2018-04-05 23:46:16 +0900

    球体のスクリプトを追加

---
+++ Unity/Assets/Script/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        // rigidbodyのx軸（横）とz軸（奥）に力を加える
+        rigidbody.AddForce(x, 0, z);
+    }
+}
